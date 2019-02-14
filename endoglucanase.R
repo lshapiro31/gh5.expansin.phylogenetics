24ea5fd297b903505fa41e503528bff3618a1816
Lori R. Shapiro <lori.r.shapiro@gmail.com>
2019-02-14 10:26:23 -0500

    the struggle is real

--- endoglucanase.R
+++ endoglucanase.R
@@ -17,46 +17,43 @@ library(readr)
 
 setwd("/Users/lorishapiro/Dropbox/AncientDNA/Expansin-endoglucanase-phylogeny/")
 annot <- read.csv("endoglucanase.gh5.annotations.R.txt", header=T, sep="\t")
+str(annot)  # Read in as a dataframe
 names(annot)
 
-raxml_file <- system.file("gh5.RAxML_bipartitions.result") ## Need to figure out how to get the bootstrap values into the tree!
-bootstrap <- read.raxml(raxml_file)
+# create new dataset without missing data 
+annot <- na.omit(annot)
 
-nwk <- ("gh5.rename.nwk")
+# RAxML output processed with sumtrees.py
+# sumtrees.py gh5-RAxML_bootstrap.result -d 2 -F newick > gh5.new.nwk
+
+nwk <- ("gh5.new.nwk") ## raxml output from sumtrees.py
 tree <- read.tree(nwk)
 str(tree)
-
-p <- ggtree(tree)
-p <- p %<+% annot
-str(p)
-dim(p)
 
 tree$node.label # Obtain internal node labels
 tree$tip.label # Obtain internal node labels
 
+## Attach annotations
+tr <- ggtree(tree)
+tr2 <- tr %<+% annot
+str(tr2)
+
+## Specify color is by taxonomy from file
+group_tree <- split(tr2$data$Class, tr2$data$Group)
+str(group_tree)
+group_tree <- groupOTU(tree, group_tree, group_name = "my_group")
+str(group_tree)
+
+trial<-ggtree(group_tree, aes(color = my_group)) #+ scale_color_manual(values = c("red", "black"))
+trial
+
 # Display internal node numbers
 ggtree(tree) + geom_text2(aes(subset=!isTip, label=node), hjust=-.3) + geom_tiplab(size=3, color="black")
 
-cls <- list(c1=(.node=c(129)), # Firmicutes
-            c1=(.node=c(140)), # Firmicutes
-            c2=(.node=c(125)), # Myxobacteria
-            c2=(.node=c(142)), # Myxobacteria
-            c3=(.node=c(147)), # Bacteroidetes
-            c4=(.node=c(90)), # Worms
-            c7=(.node=c(104)), # Alteromonadales
-            c6=(.node=c(112)), # Vibrionales
-            c6=(.node=c(106)), # Vibrionales
-            c6=(.node=c(107)), # Vibrionales
-            c5=(.node=c(116)), # Enterobacteria
-            c5=(.node=c(121)), # Enterobacteria
-            c5=(.node=c(142))) # Enterobacteria
-
-
-tree <- groupClade(tree, cls)
-
-p <- ggplot(tree, aes(color=group)) + 
+p <- ggplot(tree) + 
   geom_tree() + 
-  geom_nodelab() +
+  geom_nodelab(aes(label = label), color = "black", size=2.3) +
+#  geom_nodelab() +
   theme_tree() + 
   geom_treescale() +
   geom_tiplab(geom = "text", size=2.3, color="black") 
