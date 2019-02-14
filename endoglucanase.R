8e8cbf2e8593ca88a1e966e21a65d1442fd9c1bf
Lori R. Shapiro <lori.r.shapiro@gmail.com>
2019-02-14 14:19:10 -0500

    updated annotation file and R tree script

--- endoglucanase.R
+++ endoglucanase.R
@@ -2,7 +2,6 @@ library(treeio)
 library(ggtree)
 library(ape)
 library(ggplot2)
-library(tidytree)
 library(colorspace)
 library(purrr)
 library(tidytree)
@@ -11,6 +10,9 @@ library(dplyr)
 library(magrittr)
 library(readxl)
 library(readr)
+library(tidyverse)
+library(RColorBrewer)
+library(stringr)
 
 ## To remove attached packages 
 # lapply(paste('package:',names(sessionInfo()$otherPkgs),sep=""),detach,character.only=TRUE,unload=TRUE)
@@ -38,32 +40,33 @@ tr <- ggtree(tree)
 tr2 <- tr %<+% annot
 str(tr2)
 
+
 ## Specify color is by taxonomy from file
-group_tree <- split(tr2$data$Class, tr2$data$Group)
+group_tree <- split(tr2$data$label, tr2$data$Phylum)
 str(group_tree)
-group_tree <- groupOTU(tree, group_tree, group_name = "my_group")
+group_tree <- groupOTU(tree, group_tree, group_name = "Taxonomic_Groups")
 str(group_tree)
 
-trial<-ggtree(group_tree, aes(color = my_group)) #+ scale_color_manual(values = c("red", "black"))
-trial
 
 # Display internal node numbers
 ggtree(tree) + geom_text2(aes(subset=!isTip, label=node), hjust=-.3) + geom_tiplab(size=3, color="black")
 
-p <- ggplot(tree) + 
+#define colors for taxonomy coloring
+colors1<-c("#a6cee3", "#1f78b4", "#b15928", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#cccc3d")
+
+p <- ggplot(group_tree, aes(color = Taxonomic_Groups)) + 
   geom_tree() + 
   geom_nodelab(aes(label = label), color = "black", size=2.3) +
-#  geom_nodelab() +
   theme_tree() + 
   geom_treescale() +
-  geom_tiplab(geom = "text", size=2.3, color="black") 
+  geom_tiplab(geom = "text", size=2.3, color="black") +
+  scale_color_manual(values = colors1) +
+  theme(legend.position = "right")+
+  scale_size_manual(values=c(1, 3)) + 
+  geom_tiplab(color = "black", size = 2.3)
 p + ggtitle("Glycoside Hydrolase 5 Phylogeny") +
   theme(plot.title = element_text(lineheight=.8, face="bold", hjust=0.5)) +
-  geom_cladelabel(node=90, label="Nematodes", align=TRUE, offset = 0.25) +
-  geom_cladelabel(node=142, label="Enterobacteriaceae", align=TRUE) +
-  geom_cladelabel(node=116, label="Enterobacteriaceae", align=TRUE) +
-  geom_cladelabel(node=121, label="Enterobacteriaceae", align=TRUE) 
-
-
-
-
+ geom_cladelabel(node=137, label="Enterobacteriaceae", align=TRUE) +
+ geom_cladelabel(node=116, label="Enterobacteriaceae", align=TRUE) +
+ geom_cladelabel(node=111, label="Enterobacteriaceae", align=TRUE) 
+  
